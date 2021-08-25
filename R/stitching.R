d702933eacdde84e152f4a97d7ad7d498dccb2bf
dungates <duncan.gates123@gmail.com>
2021-08-25 16:41:48 -0700

    add to github

---
+++ R/stitching.R
@@ -0,0 +1,17 @@
+
+cheatsheets_page <- rvest::read_html("https://github.com/rstudio/cheatsheets")
+
+cheatsheets_list <- cheatsheets_page %>%
+  rvest::html_nodes(".Link--primary") %>%
+  rvest::html_text() %>%
+  stringr::str_subset(".pdf") %>%
+  stringr::str_replace_all(" ", "%20")
+  
+
+purrr::walk(cheatsheets_list, ~ download.file(url = paste0("https://github.com/rstudio/cheatsheets/raw/master/", .x),
+              destfile = paste0(here::here("download/"), .x)))
+
+staplr::staple_pdf(
+  input_directory = here("download"),
+  output_filepath = here("all_cheatsheets")
+)
